1fa57fca6def3c70e2a871a50514d0dc123e8a4d
axelrivero <ing.axelr@gmail.com>
2023-08-30 14:24:52 -0400

    Se eliminaron algunas líneas de código

--- Dev_Pokedex_V0.1.py
+++ Dev_Pokedex_V0.1.py
@@ -7,7 +7,7 @@ import requests
 
 
 # Estas dos primeras funciones podemos volverla uno sola
-#  que piensas solo cambiando la clave a usar en la api del URI
+#   solo cambiando la clave a usar en el URI del API
 
 def obtener_informacion_pokemon(nombre):
     """busco al pokemon en la pokedex por su nombre"""
@@ -65,18 +65,6 @@ def crear_nuevo_pokemon(datos_pokemon):
         print("No se pudo crear el nuevo Pokémon.")
         return None
     
-def crear_nuevo_pokemon(datos_pokemon):
-    """Crea un nuevo Pokémon en la Pokédex."""
-
-    url = "https://pokeapi.co/api/v2/pokemon"
-    respuesta = requests.post(url, json=datos_pokemon, timeout=15)
-
-    if respuesta.status_code == 201:
-        nuevo_pokemon = respuesta.json()
-        return nuevo_pokemon
-    else:
-        print("No se pudo crear el nuevo Pokémon.")
-        return None
     
 def eliminar_pokemon(identificador):
     """Elimina un Pokémon de la Pokédex."""
@@ -241,42 +229,3 @@ def menu_principal():
 # Llamar a la función principal del menú
 menu_principal()
 
-
-"""
-# Interacción con el usuario
-busqueda = input("Ingrese el nombre o número de la Pokédex del Pokémon que desea buscar: ")
-
-# Llamar a la función de solicitud
-if busqueda.isnumeric():
-    pokemon = obtener_informacion_pokemon_por_numero(busqueda)
-else:
-    pokemon = obtener_informacion_pokemon(busqueda.lower())
-
-# Procesar y mostrar los resultados
-if pokemon:
-    print("Información del Pokémon:")
-    print(f"Nombre: {pokemon['name']}")
-    print(f"Altura: {pokemon['height']} cm")
-    print(f"Peso: {pokemon['weight']} kg")
-    # Obtener los tipos de Pokémon
-    tipos = []
-    for tipo in pokemon['types']:
-        tipos.append(tipo['type']['name'])
-
-    # Imprimir los tipos de Pokémon
-    dos_tipos = ", ".join(tipos)
-    print(f"Tipos: {dos_tipos}")
-
-    datos_pokemon = {
-        "Nombre": pokemon['name'],
-        "Altura": pokemon['height'],
-        "Peso": pokemon['weight'],
-        "Tipos": [tipo['type']['name'] for tipo in pokemon['types']]
-    }
-
-    # Guardar el Pokémon en un archivo JSON
-    save_from_json(datos_pokemon, "pokedex_usuario.json")
-
-    print("El Pokémon se ha guardado en el archivo la Pokedex del usuario.")
-
-    """
